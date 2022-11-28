01f99dc1ec5add70a70551bf171a4b1c317044d6
Diane <dianeyasminec60@gmail.com>
2022-11-28 10:56:37 -0500

    process

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,28 +1,39 @@
 import React,{useState, useEffect} from 'react'
-import { Button, Container } from 'react-bootstrap';
+import { Button, Container, Form } from 'react-bootstrap';
 
 export default function Home() {
 
 
     const [isDark, setIsDark] = useState(false)
     const appClass = isDark ? "App dark" : "App light"
-    const [weather, setWeather] = useState()
+    const [weather, setWeather] = useState([])
+    const [cityName, setCityName] = useState(' ')
     
 
 useEffect(()=>{
  
   const API_key = 'b13949e909670fa4142b477738e1435a';
-  const city = 'Houston'
+  //  const city = 'Houston'
 
-  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_key}`)
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_key}`)
     .then(response => response.json())
     .then(data => setWeather(data))
     .catch(err => console.error(err));
-},[])    
+},[cityName])    
 console.log(weather)
 
+function handleChange(e){
+ 
+ setCityName(e.target.value)
+ console.log(cityName)
+}
 function handleOnClick(){
   setIsDark((isDark)=> !isDark)
+}
+
+function handleSubmit(e){
+  e.preventDefault()
+  console.log('submit')
 }
 
   return (
@@ -36,6 +47,25 @@ function handleOnClick(){
             onClick={handleOnClick}>
       {isDark? 'Dark Mode' : 'Light Mode'}
       </Button>
+      </div>
+
+      <div>
+      <Form 
+ 
+ className="d-flex text-center m-5 form">
+            <Form.Control
+            onSubmit= {handleSubmit}
+            size="lg"
+             placeholder='Enter City'
+            value =
+            {cityName}
+             onChange={handleChange}
+              type="search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="dark">Search</Button>
+          </Form>
       </div>
    
 <Container > 
